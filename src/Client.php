c6091d253abb3dc93a38736e0c042d48e8a0e293
64kbytes <64kgames@gmail.com>
2017-04-24 17:20:05 -0300

    Coding Client class

--- src/Client.php
+++ src/Client.php
@@ -2,10 +2,25 @@
 
 namespace Baytree\Slack;
 
+use \GuzzleHttp\Client as HTTPClient;
+
 class Client
 {
-	public function __construct()
+	public function __construct(HTTPClient $HTTPClient)
 	{
+		$this->http = $HTTPClient;
+	}
 
+	public function send()
+	{
+		$r = $this->http->post('https://hooks.slack.com/services/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', [
+            'json' => [
+                "text" => "This is a test message",
+                //"channel" => "#devops",
+                "link_names" => 1, 
+                "username" => "myacobs", 
+                "icon_emoji" => ":monkey_face:"
+            ]
+        ]);
 	}
 }
