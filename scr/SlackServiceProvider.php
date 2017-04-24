1d75efd9905a9efc19d6a5df22addd9d21919716
64kbytes <64kgames@gmail.com>
2017-04-24 15:31:36 -0300

    Initial commit

---
+++ scr/SlackServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Baytree\Slack;
+
+use Illuminate\Support\ServiceProvider;
+
+class SlackServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+        require __DIR__ . '/../vendor/autoload.php';
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        // register package events
+        //$this->app->register('Baytree\KnowItFirst\EventServiceProvider');
+    }
+}
